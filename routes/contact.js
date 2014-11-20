67df078601d5722ebccdc095a8819dfa80edd210
webber_wu <webber_wu@hiiir.com>
2014-11-20 21:23:32 +0800

    增加客戶寄信及確認回覆給客戶的信件寄送

--- routes/contact.js
+++ routes/contact.js
@@ -16,6 +16,7 @@ router.post('/', function(req, res) {
 		contactMessage = req.body.message || 'NO_MWSSAGE';
 
 	var mailContent = fs.readFileSync(__dirname + '/../mailTemplates/contact.html').toString();
+	var mailConfirmation = fs.readFileSync(__dirname + '/../mailTemplates/confirmation.html').toString();
 
 	var message = {
 		html: util.format(mailContent, contactName, contactEmail, contactSubject, contactMessage),
@@ -50,10 +51,59 @@ router.post('/', function(req, res) {
 		]
 	};
 
+	var messageConfirmation = {
+		html: util.format(mailConfirmation, contactName, contactSubject, contactMessage),
+		subject: 'EXMA WEBSIDE CONTACT',
+		from_email: 'kevinxyz77@gmail.com',
+		from_name: 'KEVIN - Project Manager',
+		to: [
+			{
+				email: contactEmail,
+				type: 'to'
+			},
+			{
+				email: 'kevinxyz77@gmail.com',
+				type: 'bcc'
+			},
+			{
+				email: 'blackghost0118@gmail.com',
+				type: 'bcc'
+			},
+			{
+				email: 'clonncd@gmail.com',
+				type: 'bcc'
+			},
+			{
+				email: 'whsh320@gmail.com',
+				type: 'bcc'
+			},
+			{
+				email: 'wayne1025tw@gmail.com',
+				type: 'bcc'
+			},
+			{
+				email: 'bird23074035@gmail.com',
+				type: 'bcc'
+			}
+		]
+	};
+
 	mandrillClient.messages.send({ 'message': message }, 
 		function (result) {
 			console.log(result);
-			res.redirect('/');
+			mandrillClient.messages.send({ 'message': messageConfirmation }, 
+				function (result) {
+					console.log(result);
+					res.redirect('/');
+					return;
+				},
+				function (err){
+					console.log('err');
+					console.log(err);
+					res.redirect('/');
+					return;
+				}
+			);
 			return;
 		},
 		function (err){
