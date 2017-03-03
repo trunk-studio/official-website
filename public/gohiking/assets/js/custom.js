ac34cdea18fc85f8388d1fb3538d1ae1216a5f1c
Kuyen <iamcxa@gmail.com>
2017-03-03 16:19:45 +0800

    add privacy-policy page and modal

--- public/gohiking/assets/js/custom.js
+++ public/gohiking/assets/js/custom.js
@@ -22,68 +22,69 @@ $(document).ready(function() {
     // Fullpage init
     $('#fullpage').fullpage({
 
-      // Navigation
-      menu: ".menu",
+        // Navigation
+        menu: ".menu",
 
-      // Scrolling
-      scrollingSpeed: 700,
-      easingcss3: "cubic-bezier(0.39, 0.575, 0.565, 1)", // easeOutSine
+        // Scrolling
+        scrollingSpeed: 700,
+        easingcss3: "cubic-bezier(0.39, 0.575, 0.565, 1)", // easeOutSine
 
-      //Custom selectors
-      sectionSelector: 'section',
-      slideSelector: 'slide_custom',
+        //Custom selectors
+        sectionSelector: 'section',
+        slideSelector: 'slide_custom',
 
-      // Design
-      paddingTop: "130px",
-      paddingBottom: "50px",
-      responsiveWidth: 768,
+        // Design
+        paddingTop: "130px",
+        paddingBottom: "50px",
+        responsiveWidth: 768,
 
-      // Events
-      afterLoad: function(anchorLink, index) {
+        // Events
+        afterLoad: function(anchorLink, index) {
 
-        // Background color switcher
-        backgroundColor(index - 1);
+            // Background color switcher
+            backgroundColor(index - 1);
 
-        // Animate elements
-        animateElements(index - 1, true);
+            // Animate elements
+            animateElements(index - 1, true);
 
-        // Enable/disable carousel
-        if (anchorLink == "section_carousel") {
-          $(".carousel").carousel("cycle");
-        } else {
-          $(".carousel").carousel("pause");
+            // Enable/disable carousel
+            if (anchorLink == "section_carousel") {
+                $(".carousel").carousel("cycle");
+            } else {
+                $(".carousel").carousel("pause");
+            }
+
+        },
+        onLeave: function(index, nextIndex, direction) {
+
+            // Background color switcher
+            backgroundColor(nextIndex - 1);
+
+            // Remove animation classes from elements on the current section
+            setTimeout(function() {
+                animateElements(index - 1, false);
+            }, 700);
+
+            // Fade out/in old/new sections
+            $(this).css("opacity", "0");
+            $("section").eq(nextIndex - 1).css("opacity", "1");
+
+            // Translate background image
+            bgTranslateY -= (nextIndex - index) * 30;
+
+            $(".bg-image").css({
+                "-webkit-transform": "translateY(" + bgTranslateY + "px)",
+                "-ms-transform": "translateY(" + bgTranslateY + "px)",
+                "transform": "translateY(" + bgTranslateY + "px)"
+            });
+
+            // Device slideshow
+            deviceSlideshow(nextIndex - 1);
+
         }
 
-      },
-      onLeave: function(index, nextIndex, direction) {
-
-        // Background color switcher
-        backgroundColor(nextIndex - 1);
-
-        // Remove animation classes from elements on the current section
-        setTimeout(function() {
-          animateElements(index - 1, false);
-        }, 700);
-
-        // Fade out/in old/new sections
-        $(this).css("opacity", "0");
-        $("section").eq(nextIndex - 1).css("opacity", "1");
-
-        // Translate background image
-        bgTranslateY -= (nextIndex - index) * 30;
-
-        $(".bg-image").css({
-          "-webkit-transform": "translateY(" + bgTranslateY + "px)",
-              "-ms-transform": "translateY(" + bgTranslateY + "px)",
-                  "transform": "translateY(" + bgTranslateY + "px)"
-        });
-
-        // Device slideshow
-        deviceSlideshow(nextIndex - 1);
-
-      }
-
     });
+
 });
 
 
@@ -92,16 +93,16 @@ $(document).ready(function() {
  */
 
 function animateElements(section, animate) {
-  $("section").eq(section).find("[data-animate-in]").each(function() {
-    var animateClass = $(this).data("animate-in");
+    $("section").eq(section).find("[data-animate-in]").each(function() {
+        var animateClass = $(this).data("animate-in");
 
-    // Add classes if animate == true, remove if false
-    if (animate) {
-      $(this).addClass("animated " + animateClass);
-    } else {
-      $(this).removeClass("animated " + animateClass);
-    }
-  });
+        // Add classes if animate == true, remove if false
+        if (animate) {
+            $(this).addClass("animated " + animateClass);
+        } else {
+            $(this).removeClass("animated " + animateClass);
+        }
+    });
 };
 
 
@@ -111,11 +112,11 @@ function animateElements(section, animate) {
 
 $(function() {
 
-  var bg = $(".bg-image");
-  var sections = $("section").length;
-  var bottomOffset = 30 * sections;
+    var bg = $(".bg-image");
+    var sections = $("section").length;
+    var bottomOffset = 30 * sections;
 
-  bg.css("bottom", -bottomOffset + "px");
+    bg.css("bottom", -bottomOffset + "px");
 
 });
 
@@ -125,14 +126,14 @@ $(function() {
  */
 
 $(function() {
-  $(".navbar").on({
-    "show.bs.collapse": function() {
-      $(this).addClass("navbar-mobile");
-    },
-    "hide.bs.collapse": function() {
-      $(this).removeClass("navbar-mobile");
-    }
-  });
+    $(".navbar").on({
+        "show.bs.collapse": function() {
+            $(this).addClass("navbar-mobile");
+        },
+        "hide.bs.collapse": function() {
+            $(this).removeClass("navbar-mobile");
+        }
+    });
 });
 
 
@@ -141,11 +142,12 @@ $(function() {
  */
 
 $(function() {
-  $(".navbar-collapse li > a").click(function() {
-    if ($(".navbar-collapse").attr("aria-expanded") && !$(this).hasClass("dropdown-toggle")) {
-      $(".navbar-collapse").collapse("hide");
-    }
-  });
+    $(".navbar-collapse li > a").click(function(e) {
+        console.log(e)
+        if ($(".navbar-collapse").attr("aria-expanded") && !$(this).hasClass("dropdown-toggle")) {
+            $(".navbar-collapse").collapse("hide");
+        }
+    });
 });
 
 
@@ -154,10 +156,10 @@ $(function() {
  */
 
 function backgroundColor(index) {
-  var colors = $(".bg-colors").data("colors");
-  colors = colors.split(",");
+    var colors = $(".bg-colors").data("colors");
+    colors = colors.split(",");
 
-  $(".bg-colors").css("background-color", colors[index]);
+    $(".bg-colors").css("background-color", colors[index]);
 };
 
 
@@ -165,12 +167,12 @@ function backgroundColor(index) {
  * Initialize popovers & tooltips
  */
 
-$(function () {
-  $('[data-toggle="popover"]').popover();
+$(function() {
+    $('[data-toggle="popover"]').popover();
 });
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
 
@@ -180,16 +182,34 @@ $(function () {
 
 function deviceSlideshow(index) {
 
-  $(".device__slides").each(function() {
-    var fullHeight = $(this).height(),
-        totalSlides = $(this).children("img").length,
-        singleSlideHeight = fullHeight / totalSlides,
-        shiftScreensBy = -(index * singleSlideHeight);
+    $(".device__slides").each(function() {
+        var fullHeight = $(this).height(),
+            totalSlides = $(this).children("img").length,
+            singleSlideHeight = fullHeight / totalSlides,
+            shiftScreensBy = -(index * singleSlideHeight);
 
-    $(this).css({
-      "-webkit-transform": "translateY(" + shiftScreensBy + "px)",
-          "-ms-transform": "translateY(" + shiftScreensBy + "px)",
-              "transform": "translateY(" + shiftScreensBy + "px)"
-    });  
-  });
+        $(this).css({
+            "-webkit-transform": "translateY(" + shiftScreensBy + "px)",
+            "-ms-transform": "translateY(" + shiftScreensBy + "px)",
+            "transform": "translateY(" + shiftScreensBy + "px)"
+        });
+    });
 };
+
+/**
+ *  privacy-policy modal dialog
+ */
+$(function() {
+    $("#privacy-policy").off();
+    $("#privacy-policy").on("click", function() {
+        var dialog = bootbox.dialog({
+            title: '台灣步道一指通 - 隱私權政策',
+            message: '<div id="privacy-policy-reader"></div>'
+        });
+        dialog.init();
+
+        PDFObject.embed("assets/privacy-policy.pdf", "#privacy-policy-reader");
+        $('#privacy-policy-reader').css('height', $(window).height() * 0.8);
+        $('.modal-dialog').css('width', '80%');
+    });
+});
